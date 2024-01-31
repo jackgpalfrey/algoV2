3f2231457a99932327cc335e3120b14141909b9c
Jack Palfrey <jackgpalfrey@outlook.com>
2024-01-31 19:47:13 +0000

    feat: add better home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
-<h1>Welcome to AlgoV</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<ul>
-	<li><a href="sorting/bubble">Bubble Sort</a></li>
-</ul>
+<main class="p-3">
+	<h1 class="text-3xl">Welcome to AlgoV</h1>
+	<br />
+
+	<h2 class="mb-1 text-xl">Sorting Algorithms</h2>
+	<ul class="list-disc pl-6">
+		<li><a class="text-red-400" href="sorting/bubble">Bubble Sort</a></li>
+		<li><a class="text-red-400" href="sorting/insertion">Insertion Sort</a></li>
+	</ul>
+</main>
