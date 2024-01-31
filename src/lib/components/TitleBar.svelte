4ec1b5897da54c71d7c9f5d9a495125331d96335
Jack Palfrey <jackgpalfrey@outlook.com>
2024-01-31 20:03:34 +0000

    feat: make title bar go home

--- src/lib/components/TitleBar.svelte
+++ src/lib/components/TitleBar.svelte
@@ -4,5 +4,5 @@
 
 <title>{breadcrumb[breadcrumb.length - 1]}</title>
 <header class="flex h-[5%] items-center justify-center">
-	<h1 class="text-center text-2xl text-red-500">{breadcrumb.join(' - ')}</h1>
+	<h1 class="text-center text-2xl text-red-500"><a href="/">{breadcrumb.join(' - ')}</a></h1>
 </header>
